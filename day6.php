dd7f4e8f7b85855e7f36420f13103bcdb71b8a20
Raju <razutimalsina123@gmail.com>
2025-10-27 21:57:47 +0545

    day 6 course added

---
+++ day6.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <!-- form handling -->
+        <form action="rabina.php" method='GET'>
+            <label for="name">Name: </label>
+            <input type="mobile" name="name" id=""><br><br>
+
+            <label for="mobile">Mobile Number: </label>
+            <input type="number" name="mobile" id=""><br><br>
+
+            <label for="email">Email :</label>
+            <input type="email" name="email" id=""><br><br>
+
+            <input type="submit" value="submit">
+        </form>
+
+</body>
+</html>
---
+++ rabina.php
@@ -0,0 +1,5 @@
+<?php
+
+    echo "Welcome ".$_GET['name']."<br> Your Mobile Number is ".$_GET['mobile']."<br> Your email is ".$_GET['email']
+
+?>
